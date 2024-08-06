7b37e22f52067699ffbd12dd55cc2815ade17aa8
Rhythm555 <rhythmrana36@gmail.com>
2024-08-06 11:37:51 +0530

    Update functions.sol

--- functions.sol
+++ functions.sol
@@ -1,25 +1,54 @@
-//SPDX-License-Identifier: MIT 
+//SPOX-License-Identifier: MIT 
 pragma solidity 0.8.18;
 
+/*
+
+REQUIREMENTS
+
+1. Your contract will have public variables that store the details about your coin (Token Name, Token Aborv., Total Supply)
+
+2. Your contract will have a mapping of addresses to balances (address => wint) 3. You will have a mint function that takes two parameters: an address and a value.
+
+3. The function then increases the total supply by that number and increases the balance of the "sender" address by that amount
+
+4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens.It will take an address and value just like the mint functions. It will then deduct the value from the total supplyand from the balance of the "sender".
+
+5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal to the amount that is supposed to be burned.
+*/
+
 contract MyToken {
-    // Public variables
-    string public tokenName = "scifi"; 
-    string public tokenSymbol = "sf";
-    uint public totalSupply = 100;
 
-    // Mapping for balances
-    mapping(address => uint) public balances;
+// public variables here
 
-    // Mint function
-    function mint(address _to, uint _value) external {
-        totalSupply += _value;
-        balances[_to] += _value;
-    }
+string public tname = "scifi"; 
+string public tabbry = "sf";
 
-    // Burn function
-    function burn(address _from, uint _value) external {
-        require(balances[_from] >= _value, "Insufficient balance");
-        totalSupply -= _value;
-        balances[_from] -= _value;
-    }
+uint public tsupply= 100;
+
+// mapping variable here
+
+mapping (address => uint) public assesti;
+
+// mint function
+
+I
+
+function mint(address abc, uint value) external {
+
+tsupply += value;
+
+assest [abc] +=value;
+
+// burn function
+
+function burn(address abc, uint value) external {
+
+if(assest[abc]= value) {
+    tsupply -= value;
+
+assest[abc]-= value;
+
+}
+}
+
 }
